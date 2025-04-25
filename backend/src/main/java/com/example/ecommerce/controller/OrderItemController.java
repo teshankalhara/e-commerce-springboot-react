850cc013328453b86e5594f606ed7745f78eeb2d
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-25 14:19:55 +0530

    Add documentation comments to OrderItemController class

--- backend/src/main/java/com/example/ecommerce/controller/OrderItemController.java
+++ backend/src/main/java/com/example/ecommerce/controller/OrderItemController.java
@@ -1,3 +1,10 @@
+/*
+    Order item controller
+    
+    @author teshan_kalhara
+    @create 4/25/2025
+    @update 4/25/2025
+*/
 package com.example.ecommerce.controller;
 
 public class OrderItemController {
