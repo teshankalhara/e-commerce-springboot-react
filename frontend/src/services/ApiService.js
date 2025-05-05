608826152943d5bb310b6016f3651a2a477efdb8
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 22:50:50 +0530

    refactor: simplify return statements and remove unnecessary semicolons in ApiService

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -7,35 +7,35 @@
  * @created 5/5/2025
  * @updated 5/5/2025
  */
-import axios from "axios";
+import axios from "axios"
 
 export default class ApiService {
-    static BASE_URL = "http://localhost:8080";
+    static BASE_URL = "http://localhost:8080"
 
     static getHeader() {
         const token = localStorage.getItem("token");
         return {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json"
-        };
+        }
     }
 
     /**AUTh && USERS API */
     static async registerUser(registration) {
         const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
-        return response.data;
+        return response.data
     }
 
     static async loginUser(loginDetails) {
         const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
-        return response.data;
+        return response.data
     }
 
     static async getLoggedInUserInfo() {
         const response = await axios.get(`${this.BASE_URL}/user/my-info`, {
             headers: this.getHeader()
-        });
-        return response.data;
+        })
+        return response.data
     }
 
     /**PRODUCT ENDPOINT */
@@ -45,8 +45,8 @@ export default class ApiService {
                 ...this.getHeader(),
                 "Content-Type": "multipart/form-data"
             }
-        });
-        return response.data;
+        })
+        return response.data
     }
 
     static async updateProduct(formData) {
@@ -55,37 +55,37 @@ export default class ApiService {
                 ...this.getHeader(),
                 "Content-Type": "multipart/form-data"
             }
-        });
-        return response.data;
+        })
+        return response.data
     }
 
     static async getAllProducts() {
         const response = await axios.get(`${this.BASE_URL}/product/get-all`)
-        return response.data;
+        return response.data
     }
 
     static async searchProducts(searchValue) {
         const response = await axios.get(`${this.BASE_URL}/product/search`, {
             params: { searchValue }
-        });
-        return response.data;
+        })
+        return response.data
     }
 
     static async getAllProductsByCategoryId(categoryId) {
         const response = await axios.get(`${this.BASE_URL}/product/get-by-category-id/${categoryId}`)
-        return response.data;
+        return response.data
     }
 
     static async getProductById(productId) {
         const response = await axios.get(`${this.BASE_URL}/product/get-by-product-id/${productId}`)
-        return response.data;
+        return response.data
     }
 
     static async deleteProduct(productId) {
         const response = await axios.delete(`${this.BASE_URL}/product/delete/${productId}`, {
             headers: this.getHeader()
-        });
-        return response.data;
+        })
+        return response.data
     }
 
     /**CATEGORY */
@@ -93,31 +93,31 @@ export default class ApiService {
         const response = await axios.post(`${this.BASE_URL}/category/create`, body, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     static async getAllCategory() {
         const response = await axios.get(`${this.BASE_URL}/category/get-all`)
-        return response.data;
+        return response.data
     }
 
     static async getCategoryById(categoryId) {
         const response = await axios.get(`${this.BASE_URL}/category/get-category-by-id/${categoryId}`)
-        return response.data;
+        return response.data
     }
 
     static async updateCategory(categoryId, body) {
         const response = await axios.put(`${this.BASE_URL}/category/update/${categoryId}`, body, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     static async deleteCategory(categoryId) {
         const response = await axios.delete(`${this.BASE_URL}/category/delete/${categoryId}`, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     /**ORDEDR */
@@ -125,14 +125,14 @@ export default class ApiService {
         const response = await axios.post(`${this.BASE_URL}/order/create`, body, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     static async getAllOrders() {
         const response = await axios.get(`${this.BASE_URL}/order/filter`, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     static async getOrderItemById(itemId) {
@@ -140,7 +140,7 @@ export default class ApiService {
             headers: this.getHeader(),
             params: {itemId}
         })
-        return response.data;
+        return response.data
     }
 
     static async getAllOrderItemsByStatus(status) {
@@ -148,7 +148,7 @@ export default class ApiService {
             headers: this.getHeader(),
             params: {status}
         })
-        return response.data;
+        return response.data
     }
 
     static async updateOrderitemStatus(orderItemId, status) {
@@ -156,7 +156,7 @@ export default class ApiService {
             headers: this.getHeader(),
             params: {status}
         })
-        return response.data;
+        return response.data
     }
 
     /**ADDRESS */
@@ -164,7 +164,7 @@ export default class ApiService {
         const response = await axios.post(`${this.BASE_URL}/address/save`, body, {
             headers: this.getHeader()
         })
-        return response.data;
+        return response.data
     }
 
     /***AUTHEMNTICATION CHECKER */
