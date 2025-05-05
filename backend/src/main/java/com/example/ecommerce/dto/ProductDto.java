55e179a6f95604ad52b6bbd5b6c81c128b7a63ad
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 23:33:21 +0530

    Refactor ProductDto to improve structure and add missing annotations

--- backend/src/main/java/com/example/ecommerce/dto/ProductDto.java
+++ backend/src/main/java/com/example/ecommerce/dto/ProductDto.java
@@ -1,5 +1,30 @@
+/*
+    Product dto
+    
+    @author teshan_kalhara
+    @create 4/25/2025
+    @update 5/5/2025
+*/
 package com.example.ecommerce.dto;
 
-public class ProductDto {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+    private CategoryDto category;
 }
