8f23956be4bc0e6d62eaded731eb942db2245aa9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 23:06:24 +0530

    feat: restructure Test component to include routing and context provider

--- frontend/src/components/pages/Test.jsx
+++ frontend/src/components/pages/Test.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import Footer from '../common/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from '../common/Footer';
+import LoginPage from './LoginPage';
+import Home from './Home';
+import { CartProvider } from '../context/CartContext';
+import RegisterPage from './RegisterPage';
 
 const Test = () => {
   return (
-    <BrowserRouter>
-        <Footer/>
-    </BrowserRouter>
-  )
-}
+    <Router>
+      <CartProvider>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage/>}/>
+        </Routes>
+        <Footer />
+      </CartProvider>
+    </Router>
+  );
+};
 
-export default Test
+export default Test;
