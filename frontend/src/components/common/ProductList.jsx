efd45fa91e242afc1eebd2f0762aa8308f77741a
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 22:50:58 +0530

    refactor: remove unnecessary semicolons and streamline code in ProductList component

--- frontend/src/components/common/ProductList.jsx
+++ frontend/src/components/common/ProductList.jsx
@@ -7,34 +7,34 @@
  * @created 5/5/2025
  * @updated 5/5/2025
  */
-import React from "react";
-import { Link } from "react-router-dom";
-import { useCart } from "../context/CartContext";
+import React from "react"
+import { Link } from "react-router-dom"
+import { useCart } from "../context/CartContext"
 
 const ProductList = ({ products }) => {
-    const { cart, dispatch } = useCart();
+    const { cart, dispatch } = useCart()
 
     const addToCart = (product) => {
-        dispatch({ type: 'ADD_ITEM', payload: product });
-    };
+        dispatch({ type: 'ADD_ITEM', payload: product })
+    }
 
     const incrementItem = (product) => {
-        dispatch({ type: 'INCREMENT_ITEM', payload: product });
-    };
+        dispatch({ type: 'INCREMENT_ITEM', payload: product })
+    }
 
     const decrementItem = (product) => {
-        const cartItem = cart.find(item => item.id === product.id);
+        const cartItem = cart.find(item => item.id === product.id)
         if (cartItem && cartItem.quantity > 1) {
-            dispatch({ type: 'DECREMENT_ITEM', payload: product });
+            dispatch({ type: 'DECREMENT_ITEM', payload: product })
         } else {
-            dispatch({ type: 'REMOVE_ITEM', payload: product });
+            dispatch({ type: 'REMOVE_ITEM', payload: product })
         }
-    };
+    }
 
     return (
         <div className="flex flex-wrap gap-5 justify-center px-20 py-6">
             {products.map((product, index) => {
-                const cartItem = cart.find(item => item.id === product.id);
+                const cartItem = cart.find(item => item.id === product.id)
                 return (
                     <div
                         key={index}
@@ -76,10 +76,10 @@ const ProductList = ({ products }) => {
                             </button>
                         )}
                     </div>
-                );
+                )
             })}
         </div>
-    );
-};
+    )
+}
 
-export default ProductList;
+export default ProductList
