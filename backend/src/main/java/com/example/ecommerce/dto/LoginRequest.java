cc9440508b9ea5478f422e84ccdb1a91b1b724d0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 23:32:57 +0530

    Update LoginRequest DTO with validation annotations

--- backend/src/main/java/com/example/ecommerce/dto/LoginRequest.java
+++ backend/src/main/java/com/example/ecommerce/dto/LoginRequest.java
@@ -3,10 +3,18 @@
     
     @author teshan_kalhara
     @create 4/25/2025
-    @update 4/25/2025
+    @update 5/5/2025
 */
 package com.example.ecommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
 
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
 }
