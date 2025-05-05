d350d167339030380ecbe06614d221b10dbdb9df
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 22:41:19 +0530

    feat: create Test component with Footer integration

---
+++ frontend/src/components/pages/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Footer from '../common/Footer'
+
+const Test = () => {
+  return (
+    <BrowserRouter>
+        <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default Test
