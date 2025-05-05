6f4234b684d4678321c4a154b379b8843a8c6740
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-05 23:33:29 +0530

    Update UserDto to reflect the latest modifications and improve structure

--- backend/src/main/java/com/example/ecommerce/dto/UserDto.java
+++ backend/src/main/java/com/example/ecommerce/dto/UserDto.java
@@ -3,10 +3,30 @@
     
     @author teshan_kalhara
     @create 4/25/2025
-    @update 4/25/2025
+    @update 5/5/2025
 */
 package com.example.ecommerce.dto;
 
-public class UserDto {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private List<OrderItemDto> orderItemList;
+    private AddressDto address;
 }
