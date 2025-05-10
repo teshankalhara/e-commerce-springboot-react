0f1441cfe17e2104a2b85f1fde0fd422a18737c5
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-10 23:45:33 +0530

    Add AddressController for managing address operations

--- backend/src/main/java/com/example/ecommerce/controller/AddressController.java
+++ backend/src/main/java/com/example/ecommerce/controller/AddressController.java
@@ -3,10 +3,30 @@
     
     @author teshan_kalhara
     @create 4/25/2025
-    @update 4/25/2025
+    @update 5/10/2025
 */
 package com.example.ecommerce.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.dto.AddressDto;
+import com.example.ecommerce.dto.Response;
+import com.example.ecommerce.service.interf.AddressService;
+
+@RestController
+@RequestMapping("/address")
+@RequiredArgsConstructor
 public class AddressController {
 
+    private final AddressService addressService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveAndUpdateAddress(@RequestBody AddressDto addressDto) {
+        return ResponseEntity.ok(addressService.saveAndUpdateAddress(addressDto));
+    }
 }
