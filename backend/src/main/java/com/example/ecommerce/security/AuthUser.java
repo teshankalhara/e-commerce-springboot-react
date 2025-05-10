1a2a930378189af4422a08f31f8b579358ad3c40
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-10 23:53:55 +0530

    Add AuthUser class to represent authenticated users in the security module

---
+++ backend/src/main/java/com/example/ecommerce/security/AuthUser.java
@@ -0,0 +1,61 @@
+/*
+    AuthUser class is used to represent the authenticated user.
+
+    @author teshan_kalhara
+    @create 5/10/2025
+    @update 5/10/2025
+ */
+package com.example.ecommerce.security;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.ecommerce.entity.User;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+public class AuthUser implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
